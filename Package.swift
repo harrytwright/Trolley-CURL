752bf591224b73673319681fbaa9dd86ab8df61e
harrytwright <haroldtomwright@gmail.com>
2018-01-31 10:17:00 +0000

    Update

--- Package.swift
+++ Package.swift
@@ -7,6 +7,17 @@ let package = Package(
     name: "CURL",
     pkgConfig: "curl",
     providers: [
-        .brew(["curl"])
+        .brew(["curl"]),
+        .apt(["libcurl4-openssl-dev"])
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CURL",
+            targets: ["CURL"]
+        )
+    ],
+    targets: [
+        .target(name: "CURL")
     ]
 )
