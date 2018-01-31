602fe1a4157c4d7d9c1d4008359430704a026cf9
harrytwright <haroldtomwright@gmail.com>
2018-01-31 16:47:10 +0000

    Change from Boolean back to void
    Due to 9 times out of then returns true

--- shim.h
+++ shim.h
@@ -954,36 +954,34 @@ typedef CF_ENUM(Integer, TCURLOption) {
     CURLcode code = curl_easy_setopt(_c, _o, _v); \
     if (code != CURLE_OK && _e) { \
         *_e = curl_code_to_error(code); \
-        return false;\
     } \
-    return true;
 
-static Boolean curl_easy_set_opt_long(CURL * handle, TCURLOption option, long value, CFErrorRef *error)
+static void curl_easy_set_opt_long(CURL * handle, TCURLOption option, long value, CFErrorRef *error)
 {
     ___curl_easy_set_opt(handle, option, value, error)
 }
 
-static Boolean curl_easy_set_opt_cstr(CURL * handle, TCURLOption option, CString value, CFErrorRef *error)
+static void curl_easy_set_opt_cstr(CURL * handle, TCURLOption option, CString value, CFErrorRef *error)
 {
     ___curl_easy_set_opt(handle, option, value, error)
 }
 
-static Boolean curl_easy_set_opt_int64(CURL * handle, TCURLOption option, CInt64 value, CFErrorRef *error)
+static void curl_easy_set_opt_int64(CURL * handle, TCURLOption option, CInt64 value, CFErrorRef *error)
 {
     ___curl_easy_set_opt(handle, option, value, error)
 }
 
-static Boolean curl_easy_set_opt_slist(CURL * handle, TCURLOption option, CSList value, CFErrorRef *error)
+static void curl_easy_set_opt_slist(CURL * handle, TCURLOption option, CSList value, CFErrorRef *error)
 {
     ___curl_easy_set_opt(handle, option, value, error)
 }
 
-static Boolean curl_easy_set_opt_void(CURL * handle, TCURLOption option, AnyVoid value, CFErrorRef *error)
+static void curl_easy_set_opt_void(CURL * handle, TCURLOption option, AnyVoid value, CFErrorRef *error)
 {
     ___curl_easy_set_opt(handle, option, value, error)
 }
 
-static Boolean curl_easy_set_opt_func(CURL * handle, TCURLOption option, curl_func value, CFErrorRef *error)
+static void curl_easy_set_opt_func(CURL * handle, TCURLOption option, curl_func value, CFErrorRef *error)
 {
     ___curl_easy_set_opt(handle, option, value, error)
 }
