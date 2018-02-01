a3543eb556a10405450621b0cb157ddd6c36c46a
harrytwright <haroldtomwright@gmail.com>
2018-02-01 09:00:17 +0000

    Testing Macro

--- shim.h
+++ shim.h
@@ -40,8 +40,13 @@ static CFErrorRef curl_code_to_error(CURLcode code) {
 
 #pragma mark - Options
 
+#define __TCOption_GET_MACRO(_1, _2, _3, NAME, ...) NAME
+#define __TCOption_Anon(_name) TCURLOption##_name
+#define __TCOption_C(_name, _type) TCURLOption##_name = _type
+#define __TCOption_SWIFT_NAME(_name, _type, _swift) TCURLOption##_name CF_SWIFT_NAME(_swift) = _type
+
 /** Set the new option with the old one */
-#define TOption(_name, _curl_option) TCURLOption##_name = _curl_option
+#define TCOption(...) __TCOption_GET_MACRO(__VA_ARGS__, __TCOption_SWIFT_NAME, __TCOption_C, __TCOption_Anon, ) (__VA_ARGS__)
 
 typedef long Integer;
 
